ce18af158f702b64ff4c499e3cd7df2337b79113
Maendros <Valitsianius@hotmail.com>
2016-08-18 17:29:39 +0300

    finished comment section

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+
+	
+	  has_many :comments
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 	root to: 'articles#index'
-
-	resources :articles
+	
+	resources :articles do
+  resources :comments
+end
 	#get '/articles' => 'articles#index'
 	#get '/articles/:id' => 'articles#show', as: :article
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
