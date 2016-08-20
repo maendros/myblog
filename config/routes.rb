9d1c074c5503020c9911a334173dde2716bb4643
Maendros <Valitsianius@hotmail.com>
2016-08-20 16:06:30 +0300

    Sorcery authentication complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
    		 resources :comments
   		end
   resources :tags
-	#get '/articles' => 'articles#index'
-	#get '/articles/:id' => 'articles#show', as: :article
+   resources :authors
+
+   resources :author_sessions, only: [ :new, :create, :destroy ]
+
+get 'login'  => 'author_sessions#new'
+get 'logout' => 'author_sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
